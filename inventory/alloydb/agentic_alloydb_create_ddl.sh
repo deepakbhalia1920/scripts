9ea1e8b52a669f27ee536b8453b8e7d4151e48c6
deepakbhalia1920 <deepakbhalia1920@gmail.com>
2026-01-12 13:56:35 +0530

    Update agentic_alloydb_create_ddl.sh

--- inventory/alloydb/agentic_alloydb_create_ddl.sh
+++ inventory/alloydb/agentic_alloydb_create_ddl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # this scrit will create the DDL mentioned in sql file
-. /home/deepak_kumar214e17/agentic_config.param
+. ./agentic_config.param
 
 # Export password for psql
 export PGPASSWORD=$PASSWORD
@@ -8,5 +8,6 @@ export PGPASSWORD=$PASSWORD
 # SQL to create table
 sudo apt install postgresql-client -y
 psql --version
+
 
 psql -h $HOST -p $PORT -U $USER -d $DATABASE_NAME -f $SQL_FILE
