a20c607937a9124238661deaa7f20cc97e4f1bdf
deepakbhalia1920 <deepakbhalia1920@gmail.com>
2026-01-16 15:25:43 +0530

    Update agentic_cloudsql_create_ddl.sh

--- inventory/cloudsql_postgres/agentic_cloudsql_create_ddl.sh
+++ inventory/cloudsql_postgres/agentic_cloudsql_create_ddl.sh
@@ -11,7 +11,7 @@
 export PGPASSWORD=$PASSWORD
 
 # Start Cloud SQL Auth Proxy
-cloud-sql-proxy "$PROJECT_ID:$REGION:$INSTANCE_NAME" &
+cloud-sql-proxy --port=${PORT} "$PROJECT_ID:$REGION:$INSTANCE_NAME" &
 PROXY_PID=$!
 sleep 5  # Wait for proxy to initialize
 
@@ -25,4 +25,7 @@ if [ $? -eq 0 ]; then
 else
     echo "Error, DDL not created. hence exiting."
     exit 1
+
 fi
+
+kill $PROXY_PID
