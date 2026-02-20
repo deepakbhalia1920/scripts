cdbdc17adb296bb95d4bad866ae6a9f44cec49c4
deepakbhalia1920 <deepakbhalia1920@gmail.com>
2026-02-20 13:22:57 +0530

    Update agentic_config.sh

--- inventory/alloydb/agentic_config.sh
+++ inventory/alloydb/agentic_config.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-# --- Configuration Variables (Update these as needed) ---
-export INVENTORY_CONFIG="~/alloydb/agentic/script/agentic_config.param"
-export PROJECT_ID="dotengage" # GCP Project ID
-export REGION="us-central1" # Region where AlloyDB cluster will be created
-export CLUSTER_ID="alloydb-dev-cluster-new" # Name of the AlloyDB cluster
-export INSTANCE_ID="alloydb-dev-primary-new"  # Name of the primary instance
-export DB_PASSWORD="AlloyDB_Dev" # Password for the default database user
-export MACHINE_TYPE="n2-highmem-2" # Machine type for the instance
-export NETWORK_NAME="alloydb-network"  # VPC network name
-export ACCOUNT="deepak.kumar214e17@cognizant.com" # GCP account to use
+# --- Configuration Variables (Update these as needed) Agentic Alloydb---
+
+export PROJECT_ID=""  #Need to provide project id name here
+export REGION="us-central1" # Region where AlloyDB cluster will be created, No need to change here
+export CLUSTER_ID="" # Need to provide name of cluster eg alloydb-dev-cluster-new
+export INSTANCE_ID="" # Need to provide Primary Instance name for eg alloydb-dev-primary-new
+export DB_PASSWORD="" # Need to provide database password here.
+export MACHINE_TYPE="n2-highmem-2" # Machine type for the instance, No need to change here.
+export NETWORK_NAME="alloydb-network"  # VPC network name, No need to change here
+export ACCOUNT="" # Need to provide your account id which use to connect GCP
 
 ########################
-#echo "creating new VM instance agent-my-vw"
-#set -x
-export INSTANCE_NAME="agent-my-vm-test16"
-export ZONE_NAME="us-central1-a"
-export MACHINE_NAME="e2-medium"
-export IMAGE_FAMILY="debian-11"
-export IMAGE_PROJECT="debian-cloud"
-export TAG="ssh-access"
-export SCOPES="https://www.googleapis.com/auth/cloud-platform"
+export INSTANCE_NAME="" # Need to provide virtual instance name here eg agent-my-vm
+export ZONE_NAME="us-central1-a" # No need to change here
+export MACHINE_NAME="e2-medium" # No need to change here
+export IMAGE_FAMILY="debian-11" # No need to change here
+export IMAGE_PROJECT="debian-cloud" # No need to change here
+export TAG="ssh-access" # No need to change here
+export SCOPES="https://www.googleapis.com/auth/cloud-platform" # No need to change here
 # --- Configuration ---
-export HOST="10.0.0.11"
-export PORT="5432"
-export DATABASE_NAME="postgres"
-export SQL_FILE="agentic_alloydb_create_table.sql"
-export BUCKET="gs://alloydb-usecase/uploads"
-export USER="postgres"
-export PASSWORD="AlloyDB_Dev"
-export USERNAME="deepak_kumar214e17"
-export SCHEMA_NAME="alloydb_usecase"
-export PRIMARY_INSTANCE_ID="alloydb-dev-primary-new"
-export SRC_AGENT="/home/deepak_kumar214e17/alloydb/agentic/script/"
+export HOST="10.0.0.11" # No need to change here
+export PORT="5432" # No need to change here
+export DATABASE_NAME="" #Need to provide database name for eg postgres
+export SQL_FILE="agentic_alloydb_create_table.sql" # No need to change here
+export BUCKET="" # Need to provide Bucket path
+export USER="" # Need to provide user here for eg postgres
+export PASSWORD="" # Need to mention here password which user need to access database.
+export USERNAME="" # Need to provide username which was mentioned in home directory 
+export SCHEMA_NAME="" # Need to provide schema name for eg alloydb_usecase
+export PRIMARY_INSTANCE_ID="" # Need to provide Primary Instance name for eg alloydb-dev-primary-new
+export SRC_AGENT="" # Provide source path where scripts present for Inventory use case in cloud shell eg /home/deepak_kumar214e17/alloydb/agentic/script/
